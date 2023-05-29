77d0cba3f3527fd4a24d7a1ff0b09efc5b2b23a4
WilferAcosta <w.o.a.r@hotmail.es>
2023-05-29 17:04:14 -0500

    feat: :construction: Funciones de salida en PHP
    aprendimos a imprimir mensaje y variables en php y declaramos una variable y creamos otra para almacenar un mensaje.

--- primeraVezPHP/index.php
+++ primeraVezPHP/index.php
@@ -1,3 +1,11 @@
 <?php
-    
-?>
+    /** el echo se usa para imprimir un mensaje */
+    echo "texto a imprimir";
+    /**cremos una variable texto y la imprimimos usando el metodo printf */
+    $texto = "mundo";
+    printf("%s hola ", $texto);
+    /**creamos una variable para almacenar el mensaje que contiene el metodo sprintf que lo que hace es formatear el mensaje y sus variables */
+    $mensaje = sprintf("hola %s", $texto);
+    echo $mensaje;
+
+?>  
