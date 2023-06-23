72eceaf0daeaa0e8a479a8b388fd767f8309b152
WilferAcosta <w.o.a.r@hotmail.es>
2023-06-23 17:38:40 -0500

    feat: :rocket: borre
    borerre

--- conexionPDO/uploads/app.php
+++ conexionPDO/uploads/app.php
@@ -1,9 +1,9 @@
 <?php
 
-require"../vendor/autoload.php";
+require "../vendor/autoload.php";
 $router = new \Bramus\Router\Router();
 
-$router->get("/camper",function(){
+$router->get("/camper", function () {
     $con = new \App\connect();
     $res = $con->conx->prepare("SELECT * FROM tb_camper");
     $res->execute();
@@ -12,25 +12,37 @@ $router->get("/camper",function(){
     echo json_encode($res);
 });
 
-$router->put("/camper",function(){
-    $_DATA = json_decode(file_get_contents("php://input"),true);
+$router->post("/camper", function () {
+    $_DATA = json_decode(file_get_contents("php://input"), true);
     $con = new \App\connect();
-    $res = $con->conx->prepare("UPDATE tb_camper SET nombre = :NOMBRE WHERE id=:CEDULA");
-    $res->bindValue(":NOMBRE",$_DATA["nom"]);
-    $res->bindValue(":CEDULA",$_DATA["id"]);
+    $res = $con->conx->prepare("INSERT INTO tb_camper (nombre, id) VALUES (:NOMBRE, :CEDULA)");
+    $res->bindValue(":NOMBRE", $_DATA["nom"]);
+    $res->bindValue(":CEDULA", $_DATA["id"]);
     $res->execute();
     $res = $res->rowCount();
     echo json_encode($res);
 });
 
-$router->delete("/camper",function(){
-    $_DATA = json_decode(file_get_contents("php://input"),true);
+$router->put("/camper", function () {
+    $_DATA = json_decode(file_get_contents("php://input"), true);
     $con = new \App\connect();
-    $res = $con->conx->prepare("DELETE tb_camper WHERE id=:ID");
-    $res->bindValue(":ID",$_DATA["id"]);
+    $res = $con->conx->prepare("UPDATE tb_camper SET nombre = :NOMBRE WHERE id = :CEDULA");
+    $res->bindValue(":NOMBRE", $_DATA["nom"]);
+    $res->bindValue(":CEDULA", $_DATA["id"]);
     $res->execute();
     $res = $res->rowCount();
     echo json_encode($res);
 });
+
+$router->delete("/camper", function () {
+    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $con = new \App\connect();
+    $res = $con->conx->prepare("DELETE FROM tb_camper WHERE id = :ID");
+    $res->bindValue(":ID", $_DATA["id"]);
+    $res->execute();
+    $res = $res->rowCount();
+    echo json_encode($res);
+});
+
 $router->run();
 ?>
